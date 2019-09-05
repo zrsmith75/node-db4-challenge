720bb85a6f63600a2179c6bcba45479521126ad8
Zac Smith <zrs3141592@gmail.com>
2019-09-04 18:45:45 -0600

    basic server built and working correctly

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require("./server.js");
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 5025;
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const RecipesRouter = require("./recipes/recipes-router");
+const server = express();
+server.use(express.json());
+server.use("/api/recipes", RecipesRouter);
+
+module.exports = server;
