0a65afc8a70872e9727c0dee54f85fb38dba2961
Zac Smith <zrs3141592@gmail.com>
2019-09-04 18:54:12 -0600

    correct knexfile and db-config.js

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const server = express();
 server.use(express.json());
 server.use("/api/recipes", RecipesRouter);
 
+server.get("/", (req, res) => {
+  res.send("Welcome to / ~ server.js working!");
+});
+
 module.exports = server;
